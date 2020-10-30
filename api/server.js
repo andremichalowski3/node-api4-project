297ae413ef645969747db6afe8887b7d41ec3250
amichalo421 <andre.michalowski@gmail.com>
2020-10-30 00:19:50 -0400

    Adding environment to get request

--- api/server.js
+++ api/server.js
@@ -3,7 +3,10 @@ const server = express(); //instantiate
 // server.use(express.json()); //configure
 
 server.get('/', (req, res) => {
-	res.send(`Lambda test endpoint from server`); //test endpoint
+  res.status(200).json({
+		hello: "node 34",
+		environment: process.env.NODE_ENV
+	})
 })
 
 module.exports = server; //export
