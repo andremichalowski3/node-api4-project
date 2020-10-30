6aafcce344b7b89df5f58cf528b432d956b0d468
amichalo421 <andre.michalowski@gmail.com>
2020-10-30 00:13:14 -0400

    Test deployment changes

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 const express = require('express'); //import
 const server = express(); //instantiate
-server.use(express.json()); //configure
+// server.use(express.json()); //configure
 
 server.get('/', (req, res) => {
 	res.send(`Lambda test endpoint from server`); //test endpoint
