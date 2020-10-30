6e55d1bfa59adc914f6d8938a2429a920196ee71
amichalo421 <andre.michalowski@gmail.com>
2020-10-29 23:55:47 -0400

    Server + Test endpoint

---
+++ api/server.js
@@ -0,0 +1,9 @@
+const express = require('express'); //import
+const server = express(); //instantiate
+server.use(express.json()); //configure
+
+server.get('/', (req, res) => {
+	res.send(`Lambda test endpoint from server`); //test endpoint
+})
+
+module.exports = server; //export
